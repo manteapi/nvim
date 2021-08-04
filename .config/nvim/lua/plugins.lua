79b2259e89337b5bfb73abd30cd63f6e6648e643
Pierre-Luc <pierreluc.manteaux@gmail.com>
2021-08-04 20:56:13 +0200

    packer bootstrap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,30 @@
-vim.cmd [[packadd packer.nvim]]
+-- check if packer is installed (~/local/share/nvim/site/pack)
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
+if not packer_exists then
+	if vim.fn.input("Hent packer.nvim? (y for yada)") ~= "y" then
+		return
+	end
+
+	local directory = string.format(
+	'%s/site/pack/packer/opt/',
+	vim.fn.stdpath('data')
+	)
+
+	vim.fn.mkdir(directory, 'p')
+
+	local git_clone_cmd = vim.fn.system(string.format(
+	'git clone %s %s',
+	'https://github.com/wbthomason/packer.nvim',
+	directory .. '/packer.nvim'
+	))
+
+	print(git_clone_cmd)
+	print("Henter packer.nvim...")
+
+	return
+end
+
 return require('packer').startup(function()
-    use {'wbthomason/packer.nvim', opt = true}
+    -- use {'wbthomason/packer.nvim', opt = true}
 end)
