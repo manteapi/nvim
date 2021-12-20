ed4f4a66df27907bb49e0381432464b26efad884
Pierre-Luc <manteaux@anatoscope.com>
2021-12-20 23:07:41 +0100

    Load fzf lua requirements

--- .config/nvim/lua/telescope-settings.lua
+++ .config/nvim/lua/telescope-settings.lua
@@ -1,4 +1,24 @@
 require('telescope').setup {
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden"
+        },
+        file_ignore_patterns = {
+            "data/*",
+            ".mypy_cache/*",
+            ".venv/*",
+            "build*",
+            ".cache/.*",
+            ".git/.*"
+        }
+    },
 	extensions = {
 		fzf = {
 			fuzzy = true,                    -- false will only do exact matching
@@ -13,6 +33,7 @@ require('telescope').load_extension('fzf')
 
 local opts = { noremap = true }
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+-- Performance issue with live_grep
+-- vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
