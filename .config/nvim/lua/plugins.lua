7b9ff692625fec3bfce3c5c0e3b5c52e80df52b3
Pierre-Luc <manteaux@anatoscope.com>
2021-12-22 22:48:47 +0100

    Rename lua config files

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -40,6 +40,7 @@ return require('packer').startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+    use {'nvim-treesitter/nvim-treesitter-textobjects'}
 
     use {
         'neovim/nvim-lspconfig',
