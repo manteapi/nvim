a1891f30c9237d677d589356fe5d2c48ccf87f84
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-01-17 13:44:36 +0100

    Add keymaps for bufferline

--- .config/nvim/lua/plugins/bufferline-config.lua
+++ .config/nvim/lua/plugins/bufferline-config.lua
@@ -1,2 +1,8 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{}
+
+local opts = {noremap=true, silent=true}
+vim.api.nvim_set_keymap("n", "[b", "<cmd>:BufferLineCycleNext<CR>", opts)
+vim.api.nvim_set_keymap("n", "b]", "<cmd>:BufferLineCyclePrev<CR>", opts)
+vim.api.nvim_set_keymap("n", "gb", "<cmd>:BufferLinePick<CR>", opts)
+vim.api.nvim_set_keymap("n", "bx", "<cmd>:BufferLinePickClose<CR>", opts)
