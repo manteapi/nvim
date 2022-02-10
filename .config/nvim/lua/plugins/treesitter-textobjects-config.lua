55cc825683f19b4de8e1ddd6b3a73927b89acd04
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-02-10 09:25:17 +0100

    Fix textobjects custom syntax

--- .config/nvim/lua/plugins/treesitter-textobjects-config.lua
+++ .config/nvim/lua/plugins/treesitter-textobjects-config.lua
@@ -12,52 +12,44 @@ require'nvim-treesitter.configs'.setup {
                 ["if"] = "@function.inner",
                 ["ac"] = "@class.outer",
                 ["ic"] = "@class.inner",
-
-                -- Or you can define your own textobjects like this
-                ["iF"] = {
-                    python = "(function_definition) @function",
-                    cpp = "(function_definition) @function",
-                    c = "(function_definition) @function",
-                    java = "(method_declaration) @function",
-                },
             },
         },
-    },
-    swap = {
-        enable = true,
-        swap_next = {
-            ["<leader>a"] = "@parameter.inner",
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
         },
-        swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
         },
-    },
-    move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-        },
-    },
-    lsp_interop = {
-        enable = true,
-        border = 'none',
-        peek_definition_code = {
-            ["<leader>df"] = "@function.outer",
-            ["<leader>dF"] = "@class.outer",
+        lsp_interop = {
+            enable = true,
+            border = 'none',
+            peek_definition_code = {
+                ["<leader>df"] = "@function.outer",
+                ["<leader>dF"] = "@class.outer",
+            },
         },
     },
 }
