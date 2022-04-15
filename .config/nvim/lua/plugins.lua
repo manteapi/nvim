476ded18bd9e8ac518cff198cc92467ea1b531b0
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-15 12:59:17 +0200

    chore: remove useless code

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -50,8 +50,7 @@ return require('packer').startup(function(use)
         'kyazdani42/nvim-tree.lua',
         requires = {
             'kyazdani42/nvim-web-devicons', -- optional, for file icon
-        },
-        config = function() require'nvim-tree'.setup {} end
+        }
     }
 
     -- treesitter
