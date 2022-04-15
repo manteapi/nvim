e0cbdb7a3a82c83bbb05a805f1fd9559097e7738
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-15 13:12:57 +0200

    style: align option comments

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,20 +1,20 @@
 local options = {
-    timeoutlen = 500, -- time in milliseconds to wait for a mapped sequence to complete.
-    showmatch = true,               -- show matching
-    ignorecase = true,              -- case insensitive
-    mouse="va",                 -- a="enable mouse click" + v="middle-click paste"
-    hlsearch = true,                -- highlight search
-    incsearch = true,               -- incremental search
-    tabstop=4,               -- number of columns occupied by a tab
-    softtabstop=4,           -- see multiple spaces as tabstops so <BS> does the right thing
-    expandtab = true,               -- converts tabs to white space
-    shiftwidth=4,            -- width for autoindents
-    autoindent = true,              -- indent a new line the same amount as the line just typed
-    number = true,                  -- add line numbers
-    wildmode={"longest","list"},   -- get bash-like tab completions
-    clipboard="unnamedplus",   -- using system clipboard
-    cursorline = true,              -- highlight current cursorline
-    ttyfast = true,                 -- Speed up scrolling in Vim
+    timeoutlen = 500,            -- time in milliseconds to wait for a mapped sequence to complete.
+    showmatch = true,            -- show matching
+    ignorecase = true,           -- case insensitive
+    mouse="va",                  -- a="enable mouse click" + v="middle-click paste"
+    hlsearch = true,             -- highlight search
+    incsearch = true,            -- incremental search
+    tabstop=4,                   -- number of columns occupied by a tab
+    softtabstop=4,               -- see multiple spaces as tabstops so <BS> does the right thing
+    expandtab = true,            -- converts tabs to white space
+    shiftwidth=4,                -- width for autoindents
+    autoindent = true,           -- indent a new line the same amount as the line just typed
+    number = true,               -- add line numbers
+    wildmode={"longest","list"}, -- get bash-like tab completions
+    clipboard="unnamedplus",     -- using system clipboard
+    cursorline = true,           -- highlight current cursorline
+    ttyfast = true,              -- Speed up scrolling in Vim
     grepprg='rg --vimgrep --no-heading --smart-case',
     grepformat="%f:%l:%c:%m",
 }
