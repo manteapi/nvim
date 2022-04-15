1aa5c4ef9cb2c35edf57615941e88d9492e9dee8
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-15 12:11:17 +0200

    chore: stop using maintained option for treesitter

--- .config/nvim/lua/plugins/treesitter-config.lua
+++ .config/nvim/lua/plugins/treesitter-config.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",
+  ensure_installed = {"lua", "rust", "python", "json", "javascript"},
   sync_install = false,
   highlight = {
     enable = true
