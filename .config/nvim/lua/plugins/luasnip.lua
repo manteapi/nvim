6afa855da19b808d0f8d106edab8bbad98f70fd6
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-23 14:27:45 +0200

    feat: minimal luasnip config

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,5 +1,12 @@
 local luasnip = require "luasnip"
 
-luasnip.config.set_config{
+luasnip.config.set_config {
+    -- Remember the last snippet and allow to jump back into it
+    -- even if we moved outside of the selection
+    history = true,
 
+    -- Allow to updates dynamic snippets as we type
+    updateevents = "TextChanged,TextChangedI"
+
+    enable_autosnippets = true,
 }
