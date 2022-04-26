62ddb66e0e20bb3442c1336fc94f7be27b4f1fb9
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-26 15:41:32 +0200

    fix: better tokyonight scheme

--- .config/nvim/lua/plugins/tokyonight-config.lua
+++ .config/nvim/lua/plugins/tokyonight-config.lua
@@ -1,2 +1,12 @@
+local colors = require("tokyonight.colors").setup()
+
+vim.g.tokyonight_hide_inactive_statusline = "true"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_variables = true
+vim.g.tokyonight_lualine_bold = true
+vim.g.tokyonight_colors = {border = colors.dark3}
 vim.g.tokyonight_style = "night"
+
+vim.opt.laststatus=3
+
 vim.cmd[[colorscheme tokyonight]]
