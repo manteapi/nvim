d15f81cafdcacab86768a3b39c37039a78728838
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-27 15:44:11 +0200

    feat: add highlight on yank

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -39,3 +39,13 @@ vim.g.matchparen_insert_timeout=12  -- time ins ms to find matching parenthesis 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true})
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
