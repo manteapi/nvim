f152e170914381ef288796eaad43cc32211c77f6
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-27 23:00:38 +0200

    clean: remove unused search mapping

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,17 +1,5 @@
 -- Copied from https://vim.fandom.com/wiki/Search_for_visually_selected_text#Simple
 
--- Search for selected text, forwards or backwards.
-vim.cmd([[vnoremap <silent> * :<C-U>
-  \let old_reg=getreg('"')<Bar>let old_regtype=getregtype('"')<CR>
-  \gvy/<C-R>=&ic?'\c':'\C'<CR><C-R><C-R>=substitute(
-  \escape(@", '/\.*$^~['), '\_s\+', '\\_s\\+', 'g')<CR><CR>
-  \gVzv:call setreg('"', old_reg, old_regtype)<CR>]])
-
-vim.cmd([[vnoremap <silent> # :<C-U>
-  \let old_reg=getreg('"')<Bar>let old_regtype=getregtype('"')<CR>
-  \gvy?<C-R>=&ic?'\c':'\C'<CR><C-R><C-R>=substitute(
-  \escape(@", '?\.*$^~['), '\_s\+', '\\_s\\+', 'g')<CR><CR>
-  \gVzv:call setreg('"', old_reg, old_regtype)<CR>]])
 
 vim.keymap.set("n", "<leader><leader>r", function()
     local reload = require "plenary.reload"
