85dd5fac64e8b5a200f4341351bdebf9e09208d4
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-27 14:28:56 +0200

    feat: stop requiring confirmation on commit message

--- .config/nvim/lua/plugins/neogit-config.lua
+++ .config/nvim/lua/plugins/neogit-config.lua
@@ -1,5 +1,9 @@
 local neogit = require('neogit')
 
+neogit.setup({
+    disable_commit_confirmation = true
+})
+
 vim.api.nvim_create_user_command('NeogitOpen',
     function()
         neogit.open({ cwd = vim.fn.expand('%:p:h') })
