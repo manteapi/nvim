2a5ea21af3fc948d97954ee972575d4463921f10
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-28 23:46:59 +0200

    fix: merge color scheme colors setup to avoid overriding

--- .config/nvim/lua/plugins/tokyonight-config.lua
+++ .config/nvim/lua/plugins/tokyonight-config.lua
@@ -1,21 +1,23 @@
 vim.o.termguicolors = true
 
+vim.g.tokyonight_style = "night"
+
 local colors = require("tokyonight.colors").setup({})
 
 vim.g.tokyonight_hide_inactive_statusline = "true"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_italic_variables = true
 vim.g.tokyonight_lualine_bold = true
-vim.g.tokyonight_colors = {border = colors.dark3}
-vim.g.tokyonight_style = "night"
 vim.g.tokyonight_colors = {
     gitSigns = {
         add = colors.green,
         change = colors.blue0,
         delete = colors.red1
     },
+    border = colors.dark3
 }
 
+-- WARNING: set colorscheme after configuration
 vim.cmd[[colorscheme tokyonight]]
 
 vim.opt.laststatus=3
