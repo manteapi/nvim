f1901ae2a6551ab7c53c7bc61ec06188fbd2ad47
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-04-28 23:14:22 +0200

    feat: better colors for gitsigns

--- .config/nvim/lua/plugins/tokyonight-config.lua
+++ .config/nvim/lua/plugins/tokyonight-config.lua
@@ -8,6 +8,13 @@ vim.g.tokyonight_italic_variables = true
 vim.g.tokyonight_lualine_bold = true
 vim.g.tokyonight_colors = {border = colors.dark3}
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_colors = {
+    gitSigns = {
+        add = colors.green,
+        change = colors.blue0,
+        delete = colors.red1
+    },
+}
 
 vim.cmd[[colorscheme tokyonight]]
 
