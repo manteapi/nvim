c3a406b57c774bee6d344b7bf373d5efae93fea5
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-05-09 13:44:51 +0200

    style: align comments

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,25 +1,25 @@
 local options = {
-    timeoutlen = 400,            -- time in milliseconds to wait for a mapped sequence to complete.
-    showmatch = true,            -- show matching
-    ignorecase = true,           -- case insensitive
-    mouse="va",                  -- a="enable mouse click" + v="middle-click paste"
-    hlsearch = false,             -- highlight search
-    tabstop=4,                   -- number of columns occupied by a tab
-    softtabstop=4,               -- see multiple spaces as tabstops so <BS> does the right thing
-    expandtab = true,            -- converts tabs to white space
-    shiftwidth=4,                -- width for autoindents
-    autoindent = true,           -- indent a new line the same amount as the line just typed
-    number = true,               -- add line numbers
-    wildmode={"longest","list"}, -- get bash-like tab completions
-    clipboard="unnamedplus",     -- using system clipboard
-    cursorline = true,           -- highlight current cursorline
-    ttyfast = true,              -- Speed up scrolling in Vim
-    breakindent = true,          -- Enable breakindent
-    grepprg='rg --vimgrep --no-heading --smart-case',
+    timeoutlen = 400,                      -- time in milliseconds to wait for a mapped sequence to complete.
+    showmatch = true,                      -- show matching
+    ignorecase = true,                     -- case insensitive
+    mouse="va",                            -- a="enable mouse click" + v="middle-click paste"
+    hlsearch = false,                      -- highlight search
+    tabstop=4,                             -- number of columns occupied by a tab
+    softtabstop=4,                         -- see multiple spaces as tabstops so <BS> does the right thing
+    expandtab = true,                      -- converts tabs to white space
+    shiftwidth=4,                          -- width for autoindents
+    autoindent = true,                     -- indent a new line the same amount as the line just typed
+    number = true,                         -- add line numbers
+    wildmode={"longest","list"},           -- get bash-like tab completions
+    clipboard="unnamedplus",               -- using system clipboard
+    cursorline = true,                     -- highlight current cursorline
+    ttyfast = true,                        -- Speed up scrolling in Vim
+    breakindent = true,                    -- Enable breakindent
     grepformat="%f:%l:%c:%m",
-    relativenumber = true,       -- Show relative line number
-    signcolumn = "yes",          -- Always show signcolumn
-    autoread = true              -- Detect when a file has been changed and load it again
+    grepprg='rg --vimgrep --no-heading --smart-case',
+    relativenumber = true,                 -- Show relative line number
+    signcolumn = "yes",                    -- Always show signcolumn
+    autoread = true,                       -- Detect when a file has been changed and load it again
     directory = "~/.local/share/nvim/swap" -- NOTE: Required on my encrypted filesystem, dunno why
 }
 
