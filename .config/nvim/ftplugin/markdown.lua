5998bf7c438fb71002cf7cd0a336fcd5cd9b1395
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-05-19 09:41:33 +0200

    fix: handle escape character for pandoc

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,1 +1,5 @@
-vim.api.nvim_command("set makeprg=pandoc\\ %\\ --from\\ markdown+emoji\\ --to\\ revealjs\\ --slide-level\\ 3\\ --filter\\ pandoc-plantuml\\ --standalone\\ --output\\ index.html\\ --table-of-content\\ --variable\\ revealjs-url=https://unpkg.com/reveal.js@latest/\\ --variable\\ slideNumber=true\\ --variable\\ theme=solarized")
+local function handle_quotes(input)
+    return string.gsub(input, "'", "\\\\'")
+end
+
+vim.api.nvim_command('set makeprg=pandoc\\ '..handle_quotes(vim.fn.expand('%', '', ''))..'\\ --from\\ markdown+emoji\\ --to\\ revealjs\\ --slide-level\\ 3\\ --filter\\ pandoc-plantuml\\ --standalone\\ --output\\ index.html\\ --table-of-content\\ --variable\\ revealjs-url=https://unpkg.com/reveal.js@latest/\\ --variable\\ slideNumber=true\\ --variable\\ theme=solarized')
