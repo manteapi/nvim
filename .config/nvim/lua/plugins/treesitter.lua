0c7e81d68a1c0f302d477d5e6bc8018103a08c16
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-07-11 14:48:34 +0200

    feat: install qmljs treesitter support

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"lua", "rust", "python", "json", "javascript"},
+  ensure_installed = {"lua", "rust", "python", "json", "javascript", "qmljs"},
   sync_install = false,
   highlight = {
     enable = true
