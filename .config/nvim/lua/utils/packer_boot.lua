c606018cbe8282bb0f6c63ac5f69591ea118ca66
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-07-16 13:07:15 +0200

    feat: packer bootstrap belongs to utils module

---
+++ .config/nvim/lua/utils/packer_boot.lua
@@ -0,0 +1,85 @@
+local M = {}
+
+-- INFO: Minimalist logging which does not require any plugin. Useful for bootstrapping.
+-- However just use `vlog.nvim` and save yourself the trouble
+local echohl = vim.schedule_wrap(
+    function(msg, hl)
+        local emsg = vim.fn.escape(msg, '"')
+        vim.cmd('echohl ' .. hl .. ' | echom "' .. emsg .. '" | echohl None')
+    end
+)
+
+function M.info(msg)
+	echohl(msg, 'None')
+end
+
+function M.err(msg)
+	echohl(msg, 'ErrorMsg')
+end
+
+local function init(success, sync, startup)
+    if not success then
+        M.err('[packer]: Failed setup')
+        return
+    end
+
+    -- M.info('[packer]: Loading package list')
+    vim.cmd('packadd packer.nvim')
+
+    -- Uncomment/change this depending on where you want your 'list' of packages
+    -- to load from
+    --
+    -- This would be somewhere on your `:h runtimepath`, and would likely contain
+    -- a call to `startup` provided by `packer`
+    -- require('pack.list')
+    --
+    require('packer').startup(startup)
+    -- M.info("SYNSYNCSYCN")
+    -- M.info(sync)
+    if sync then
+	    require('packer').sync()
+    end
+end
+
+function M.bootstrap(startup)
+    -- Bootstrap `packer` installation to manage packages
+    local packer = {
+        opt_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim',
+        start_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim',
+        url = 'https://github.com/wbthomason/packer.nvim'
+    }
+    -- M.info(vim.fn.stdpath('data'))
+
+    if vim.fn.executable('git') ~= 1 then
+        M.err('[packer] Bootstrap failed, git not installed')
+        return
+    end
+
+    if vim.fn.empty(vim.fn.glob(packer.opt_path)) and vim.fn.empty(vim.fn.glob(packer.start_path)) > 0 then
+        -- M.info('[packer]: Installing...')
+
+        local handle
+        handle = vim.loop.spawn(
+            'git',
+            {
+                args = {
+                    'clone',
+                    packer.url,
+                    packer.opt_path,
+                },
+            },
+            vim.schedule_wrap(
+                function(code, _)
+                    -- Wrapper to call `init` based on the success of the above `git` operation
+                    handle:close()
+                    init(code == 0, true, startup)
+                end
+            )
+        )
+    else
+        -- `packer` already installed, continue to load package list
+        init(true, false, startup)
+    end
+end
+
+return M
