aaa37ed198836c1d23ef814f369c267fc0abf7a2
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-07-25 21:09:31 +0200

    feat: change buffer cycling mapping

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -3,7 +3,7 @@ vim.o.termguicolors = true
 require("bufferline").setup{}
 
 local opts = {noremap=true, silent=true}
-vim.api.nvim_set_keymap("n", "bk", "<cmd>:BufferLineCycleNext<CR>", opts)
-vim.api.nvim_set_keymap("n", "bj", "<cmd>:BufferLineCyclePrev<CR>", opts)
+vim.api.nvim_set_keymap("n", "bl", "<cmd>:BufferLineCycleNext<CR>", opts)
+vim.api.nvim_set_keymap("n", "bh", "<cmd>:BufferLineCyclePrev<CR>", opts)
 vim.api.nvim_set_keymap("n", "gb", "<cmd>:BufferLinePick<CR>", opts)
 vim.api.nvim_set_keymap("n", "bx", "<cmd>:BufferLinePickClose<CR>", opts)
