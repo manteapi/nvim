8ad24dd4f3971e57028a3e54508789a1227793e3
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-07-27 22:01:02 +0200

    fix: resolve conflict

--- .config/nvim/lua/config/toggleterm.lua
+++ .config/nvim/lua/config/toggleterm.lua
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-require('toggleterm').setup({})
-
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-t>', "<cmd>:ToggleTerm<CR>", opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-vim.cmd('autocmd BufWinEnter,WinEnter term://* startinsert')
-
-local opts = {noremap=true, silent=true}
-vim.api.nvim_set_keymap("n", "<C-t>", "<cmd>:ToggleTerm<CR>", opts)
-||||||| parent of b2351cc (feat: setup float direction for toggleterm)
-=======
 require('toggleterm').setup({
     direction = 'float'
 })
@@ -41,4 +19,3 @@ vim.cmd('autocmd BufWinEnter,WinEnter term://* startinsert')
 
 local opts = {noremap=true, silent=true}
 vim.api.nvim_set_keymap("n", "<C-t>", "<cmd>:ToggleTerm<CR>", opts)
->>>>>>> b2351cc (feat: setup float direction for toggleterm)
