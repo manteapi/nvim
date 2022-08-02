4f5ffc2a1e38b2e9bf86282024cf7f9aa077b2e0
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-08-02 14:12:18 +0200

    feat: better options for telescope browser

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -50,7 +50,7 @@ require('telescope').setup {
 			override_file_sorter = true,     -- override the file sorter
 			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
 			-- the default case_mode is "smart_case"
-		}
+		},
 	}
 }
 require('telescope').load_extension('fzf')
@@ -66,7 +66,11 @@ vim.keymap.set("n", "<leader>sn", function()
 end, opts)
 vim.keymap.set("n", "<leader>sf", require('telescope.builtin').find_files, opts)
 vim.keymap.set("n", "<leader>sF", require('telescope').extensions.repo.list, opts)
-vim.keymap.set("n", "<leader>se", require('telescope').extensions.file_browser.file_browser, opts)
+vim.keymap.set("n", "<leader>se", function()
+    require('telescope').extensions.file_browser.file_browser({
+        path = vim.fn.expand('%:p:h', "", "")
+    })
+end, opts)
 vim.keymap.set("n", "<leader>sB", function() require('telescope.builtin').live_grep({grep_open_files=true}) end, opts)
 vim.keymap.set("n", "<leader>ss", require('telescope.builtin').current_buffer_fuzzy_find, opts)
 vim.keymap.set("n", "<leader>sS", require('telescope.builtin').grep_string, opts)
