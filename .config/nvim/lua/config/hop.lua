ab73c374d9f83e8897047123e5b1b6adf98a2174
Pierre-Luc Manteaux <pierre-luc.manteaux@circle.dental>
2022-08-03 18:12:46 +0200

    fix: keybindings for hop were conflicting with hover lsp action

--- .config/nvim/lua/config/hop.lua
+++ .config/nvim/lua/config/hop.lua
@@ -1,8 +1,8 @@
 require('hop').setup({})
 
 local opts = {}
-vim.keymap.set('', '<leader>H', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false})<cr>", opts)
-vim.keymap.set('', '<leader>h', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false})<cr>", opts)
+vim.keymap.set('', '<leader>G', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false})<cr>", opts)
+vim.keymap.set('', '<leader>g', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false})<cr>", opts)
 vim.keymap.set('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", opts)
 vim.keymap.set('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", opts)
 vim.keymap.set('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", opts)
