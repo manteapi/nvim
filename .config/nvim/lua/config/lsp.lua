d723f0d285bb93f8c28fe39d26b2f7dfb4bfd9c3
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-10-15 20:53:30 +0200

    fix(cmp): use default_capabilities

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -53,7 +53,7 @@ end
 
 local flags = {}
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 for _, server in ipairs(servers) do
 	local server_opts = {
