acbc8a2b6b17044887fbbbe67c94ac6c88c8fd08
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-10-18 10:17:35 +0200

    style(lsp_lines): lua format

--- .config/nvim/lua/config/lsp_lines.lua
+++ .config/nvim/lua/config/lsp_lines.lua
@@ -6,16 +6,16 @@ lsp_lines.setup()
 
 -- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config({
-    virtual_text = current_virtual_text,
-    virtual_lines = current_virtual_lines
+	virtual_text = current_virtual_text,
+	virtual_lines = current_virtual_lines,
 })
 
-local opts = {noremap=true, silent=true}
+local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<Leader>l", function()
-    current_virtual_lines = not current_virtual_lines
-    current_virtual_text = not current_virtual_text
-    vim.diagnostic.config({
-        virtual_text = current_virtual_text,
-        virtual_lines = current_virtual_lines
-    })
+	current_virtual_lines = not current_virtual_lines
+	current_virtual_text = not current_virtual_text
+	vim.diagnostic.config({
+		virtual_text = current_virtual_text,
+		virtual_lines = current_virtual_lines,
+	})
 end, opts)
