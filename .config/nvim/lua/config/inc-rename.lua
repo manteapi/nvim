675c3bae8e1ed026c01a2c253d5bcd535553ce34
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-10-18 10:17:54 +0200

    style(inc_rename): lua format

--- .config/nvim/lua/config/inc-rename.lua
+++ .config/nvim/lua/config/inc-rename.lua
@@ -1,5 +1,5 @@
 require("inc_rename").setup()
 
 vim.keymap.set("n", "<leader>rn", function()
-  return ":IncRename " .. vim.fn.expand("<cword>")
+	return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
