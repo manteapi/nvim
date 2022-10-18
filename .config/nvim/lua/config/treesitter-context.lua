066661609f7ec53b4fb19aafb662e1b62d9d79b5
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-10-18 10:15:26 +0200

    refactor: switch to keymap.set

--- .config/nvim/lua/config/treesitter-context.lua
+++ .config/nvim/lua/config/treesitter-context.lua
@@ -1,2 +1,2 @@
 local opts = {noremap=true, silent=true}
-vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>TSContextToggle<cr>", opts)
+vim.keymap.set("n", "<leader>c", "<cmd>TSContextToggle<cr>", opts)
