00f6be6a756c848a734e9d336d00a74e88c1fe81
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-10-18 10:22:08 +0200

    refactor(toggleterm): switch to keymap.set

--- .config/nvim/lua/config/toggleterm.lua
+++ .config/nvim/lua/config/toggleterm.lua
@@ -1,21 +1,21 @@
-require('toggleterm').setup({
-    direction = 'float'
+require("toggleterm").setup({
+	direction = "float",
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-t>', "<cmd>:ToggleTerm<CR>", opts)
+	local bufopts = { noremap = true, buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], bufopts)
+	vim.keymap.set("t", "jk", [[<C-\><C-n>]], bufopts)
+	vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-W>h]], bufopts)
+	vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-W>j]], bufopts)
+	vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-W>k]], bufopts)
+	vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-W>l]], bufopts)
+	vim.keymap.set("t", "<C-t>", "<cmd>:ToggleTerm<CR>", bufopts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-vim.cmd('autocmd BufWinEnter,WinEnter term://* startinsert')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+vim.cmd("autocmd BufWinEnter,WinEnter term://* startinsert")
 
-local opts = {noremap=true, silent=true}
-vim.api.nvim_set_keymap("n", "<C-t>", "<cmd>:ToggleTerm<CR>", opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-t>", "<cmd>:ToggleTerm<CR>", opts)
