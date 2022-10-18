b9913308beaba02cba83e482765b70bf456cd35d
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-10-18 10:06:43 +0200

    refactor: switch to keymap.set

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -3,7 +3,7 @@ vim.o.termguicolors = true
 require("bufferline").setup{}
 
 local opts = {noremap=true, silent=true}
-vim.api.nvim_set_keymap("n", "bl", "<cmd>:BufferLineCycleNext<CR>", opts)
-vim.api.nvim_set_keymap("n", "bh", "<cmd>:BufferLineCyclePrev<CR>", opts)
-vim.api.nvim_set_keymap("n", "gb", "<cmd>:BufferLinePick<CR>", opts)
-vim.api.nvim_set_keymap("n", "bx", "<cmd>:BufferLinePickClose<CR>", opts)
+vim.keymap.set("n", "bl", "<cmd>:BufferLineCycleNext<CR>", opts)
+vim.keymap.set("n", "bh", "<cmd>:BufferLineCyclePrev<CR>", opts)
+vim.keymap.set("n", "gb", "<cmd>:BufferLinePick<CR>", opts)
+vim.keymap.set("n", "bx", "<cmd>:BufferLinePickClose<CR>", opts)
