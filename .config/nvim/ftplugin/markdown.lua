1e7ff06889c1acdf3ff9d34dbdbcdab8fb352901
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2022-11-10 23:20:00 +0100

    feat(markdown): customize pandoc make cmd

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -2,4 +2,4 @@ local function handle_quotes(input)
     return string.gsub(input, "'", "\\\\'")
 end
 
-vim.api.nvim_command('set makeprg=pandoc\\ '..handle_quotes(vim.fn.expand('%', '', ''))..'\\ --from\\ markdown+emoji\\ --to\\ revealjs\\ --slide-level\\ 3\\ --filter\\ pandoc-plantuml\\ --standalone\\ --output\\ index.html\\ --table-of-content\\ --variable\\ revealjs-url=https://unpkg.com/reveal.js@latest/\\ --variable\\ slideNumber=true\\ --variable\\ theme=solarized')
+vim.api.nvim_command('set makeprg=pandoc\\ '..handle_quotes(vim.fn.expand('%', '', ''))..'\\ --from\\ markdown+emoji\\ --to\\ revealjs\\ --slide-level\\ 2\\ --filter\\ pandoc-plantuml\\ --standalone\\ --output\\ index.html\\ --variable\\ revealjs-url=https://unpkg.com/reveal.js@latest/\\ --variable\\ slideNumber=true\\ --variable\\ theme=solarized\\ --variable\\ width=1920\\ --variable\\ height=1080')
