6281f03ff257cc6f1b44849bc3a0d6da5159168f
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2023-08-16 14:19:56 +0200

    feature: setup neorg config

--- .config/nvim/lua/config/neorg.lua
+++ .config/nvim/lua/config/neorg.lua
@@ -1,26 +1,31 @@
 require("neorg").setup({
-	load = {
-		["core.defaults"] = {},
-		["core.dirman"] = {
-			config = {
-				workspaces = {
-					work = "~/repositories/dotfiles/neo-notes/work",
-					home = "~/repositories/dotfiles/neo-notes/home",
-				},
-			},
-		},
-		["core.concealer"] = {},
-		["core.completion"] = {
-			config = {
-				engine = "nvim-cmp",
-			},
-		},
-		["core.journal"] = {
-			config = {
-				workspace = "work",
-				strategy = "flat",
+    load = {
+        ["core.defaults"] = {},
+        ["core.dirman"] = {
+            config = {
+                workspaces = {
+                    work = "~/repositories/dotfiles/neo-notes/work",
+                    home = "~/repositories/dotfiles/neo-notes/home",
+                },
+                default_workspace = "work",
+            },
+        },
+        ["core.concealer"] = {},
+        ["core.completion"] = {
+            config = {
+                engine = "nvim-cmp",
+            },
+        },
+        ["core.journal"] = {
+            config = {
+                workspace = "work",
+                strategy = "flat",
                 journal_folder = "journal"
-			},
-		},
-	},
+            },
+        },
+        ["core.pivot"] = {},
+        ["core.itero"] = {},
+        ["core.promo"] = {},
+        ["core.qol.toc"] = { config={close_after_use = true} }
+    },
 })
