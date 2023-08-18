1c9ae2aa6bca249ec72d0a38fa505277a2d83334
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2023-08-18 09:50:27 +0200

    feat: sharpen the saw: indent options & yanking highlight

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
 -- bootstrap lazy.nvim and my plugins
 require("config.options")
+require("config.autocmds")
 require("plugins.init")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,12 @@
-local options = {
-}
+vim.opt.clipboard = "unnamedplus" -- using system clipboard -> allow to copy-paste from/to outside/inside neovim
 
-for k, v in pairs(options) do
-	vim.opt[k] = v
-end
+vim.opt.tabstop = 4 -- number of columns occupied by a tab
+vim.opt.shiftwidth = 4 -- number of columns for an indent level
+vim.opt.softtabstop = 4 -- number of columns when tab or backspace are pressed
+vim.opt.expandtab = true -- converts tabs to whitespace
+vim.opt.shiftround = true -- rounds the indent spacing to the next multiple of shiftwidth -> easier indent when using `>`, `<`
+
+--Remap space as leader key
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
