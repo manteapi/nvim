237ca2707c6f41f3fabc5991eb4910589c376d3c
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2023-09-05 15:56:21 +0200

    feat: try autocmd to display macro status with cmdheight=0

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,9 +1,24 @@
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
+})
+
+
+local recording_group = vim.api.nvim_create_augroup("RecordingCmdHeight", {clear=true})
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+    callback = function()
+        vim.cmd([[set cmdheight=1]])
+    end, group=recording_group, pattern="*",
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+    callback = function()
+        vim.cmd([[set cmdheight=0]])
+    end, group=recording_group, pattern="*",
 })
