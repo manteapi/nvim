b40be4477f92dfadbe8e8468fcf6423dc6b18c62
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2023-09-13 17:57:51 +0200

    feat: setup filetype for qml

---
+++ .config/nvim/filetype.lua
@@ -0,0 +1,16 @@
+vim.filetype.add({
+    extension = {
+        qml = "qmljs"
+    },
+    filename = {
+        qmldir = "qmljs",
+    },
+})
+vim.filetype.add({
+    extension = {
+        qml = "qml"
+    },
+    filename = {
+        qmldir = "qml",
+    },
+})
