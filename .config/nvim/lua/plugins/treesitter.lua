09779289f943e030ad570fc6c071821ecde84388
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2024-02-13 10:28:17 +0100

    feat: set keymap to display status line

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,24 @@
 require'nvim-treesitter.configs'.setup {
     highlight = {enable = true},
     ensure_installed = {
-        "norg", 
-        "lua", 
-        "rust", 
-        "python", 
-        "json", 
-        "javascript", 
+        "norg",
+        "lua",
+        "rust",
+        "python",
+        "json",
+        "javascript",
         "qmljs"
     },
 }
+
+vim.keymap.set("n", "<Leader>ps", function()
+    print(require('nvim-treesitter').statusline(
+    {
+        indicator_size = 200,
+        type_patterns = {'class', 'function', 'method'},
+        transform_fn = function(line, _node) return line:gsub("%s*[%[%(%{]*%s*$", "") end,
+        separator = ' -> ',
+        allow_duplicates = false
+    }
+    ))
+end, {desc = "Show breadcrumb", silent = true, noremap = true})
