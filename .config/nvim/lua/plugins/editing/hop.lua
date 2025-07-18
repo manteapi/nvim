22272b462c99d7ab9015425f239fb6705b4717ce
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2025-07-19 00:58:59 +0200

    refactor(hop): improve keybindings

--- .config/nvim/lua/plugins/editing/hop.lua
+++ .config/nvim/lua/plugins/editing/hop.lua
@@ -1,24 +1,33 @@
-
 local hop = require('hop')
 
 hop.setup({ keys = 'etovxqpdygfblzhckisuran' })
 
 local directions = require('hop.hint').HintDirection
--- vim.keymap.set('', 'z', function()
---   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
--- end, {remap=true, silent=true})
--- vim.keymap.set('', 'Z', function()
---   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
--- end, {remap=true, silent=true})
-vim.keymap.set('', 'mf', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
-end, {remap=true, silent=true})
-vim.keymap.set('', 'mF', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-end, {remap=true, silent=true})
-vim.keymap.set('', 'mt', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
-end, {remap=true, silent=true})
-vim.keymap.set('', 'mT', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
-end, {remap=true, silent=true})
+
+vim.keymap.set('', '<leader>w', function()
+    hop.hint_words()
+end, { desc = 'Hop to word' })
+
+vim.keymap.set('', '<leader>c', function()
+    hop.hint_char1()
+end, { desc = 'Hop to 1 character' })
+
+vim.keymap.set('', '<leader>l', function()
+    hop.hint_lines()
+end, { desc = 'Hop to line' })
+
+vim.keymap.set('', 'f', function()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, { desc = 'Hop forward to char on current line' })
+
+vim.keymap.set('', 'F', function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, { desc = 'Hop backward to char on current line' })
+
+vim.keymap.set('', 't', function()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, { desc = 'Hop forward just before char on current line' })
+
+vim.keymap.set('', 'T', function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, { desc = 'Hop backward just before char on current line' })
