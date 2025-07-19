a885dbf1b8e84197f24fb095e6e9fb974dcd69cc
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2025-07-19 17:39:33 +0200

    refactor(telescope): clean up configuration

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 local telescope = require('telescope')
 
-telescope.setup{
+telescope.setup {
     defaults = {
-        path_display={
-            truncate=3
+        path_display = {
+            truncate = 3
         },
     },
     pickers = {
@@ -18,26 +18,18 @@ telescope.setup{
     },
 }
 
-function split(inputstr, sep)
+local split = function(inputstr, sep)
     if sep == nil then
         sep = "%s"
     end
-    local t={}
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
         table.insert(t, str)
     end
     return t
 end
 
-local current_selection = function()
-   local register = vim.fn.getreg("\"")
-   vim.api.nvim_exec("normal! y", false)
-   local filter = vim.fn.trim(vim.fn.getreg("@"))
-   vim.fn.setreg("\"", register)
-   return filter
-end
-
-builtin = require('telescope.builtin')
+local builtin = require('telescope.builtin')
 
 local opts = { noremap = true }
 
@@ -56,12 +48,12 @@ vim.keymap.set("n", "<Leader>tl", function()
 end, opts)
 
 vim.keymap.set("n", "<Leader>tf", function()
-    raw_args = vim.fn.input("Fd > ")
-    args = {"fd", "--hidden", "--type", "file"}
-    split_args = split(raw_args)
-    for k,v in pairs(split_args) do
+    local raw_args = vim.fn.input("Fd > ")
+    local args = { "fd", "--hidden", "--type", "file" }
+    local split_args = split(raw_args)
+    for k, v in pairs(split_args) do
         table.insert(args, v)
-        print(k,v)
+        print(k, v)
     end
     builtin.find_files({
         find_command = args
@@ -70,7 +62,7 @@ end, opts)
 
 vim.keymap.set("n", "<Leader>td", function()
     builtin.diagnostics({
-        bufnr=0
+        bufnr = 0
     })
 end, opts)
 vim.keymap.set("n", "<Leader>tt", builtin.tags, opts)
