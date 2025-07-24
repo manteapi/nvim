3a6d53839ccfc2ddd8f250da6699b2c8a2fa52d8
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2025-07-24 17:23:16 +0200

    feat(oil): show hidden files

--- .config/nvim/lua/plugins/navigation/oil.lua
+++ .config/nvim/lua/plugins/navigation/oil.lua
@@ -1,3 +1,7 @@
-require("oil").setup({})
+require("oil").setup({
+    view_options = {
+        show_hidden = true
+    }
+})
 
-vim.keymap.set("n", "<Leader>e", ":Oil<cr>", {silent = true, noremap = true, desc = "Edit filesystem in a buffer"})
+vim.keymap.set("n", "<Leader>e", ":Oil<cr>", { silent = true, noremap = true, desc = "Edit filesystem in a buffer" })
