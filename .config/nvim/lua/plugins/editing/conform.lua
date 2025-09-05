2c91732be64cb6eb0416efc6830bfbcce0baaa77
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2025-09-05 11:22:48 +0200

    fix(conform, config): filter out cpp file and add a toggle command

--- .config/nvim/lua/plugins/editing/conform.lua
+++ .config/nvim/lua/plugins/editing/conform.lua
@@ -3,8 +3,21 @@ require("conform").setup({
         python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
         qml = { "qmlformat" }
     },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-    },
+    format_on_save = function(bufnr)
+        if vim.g.disable_autoformat then
+            return
+        end
+
+        local ignore_filetypes = { "cpp" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+            return
+        end
+
+        return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
 })
+
+vim.api.nvim_create_user_command("ConformToggle", function()
+    vim.g.disable_autoformat = not vim.g.disable_autoformat
+    print("Conform autoformat " .. (vim.g.disable_autoformat and "disabled" or "enabled"))
+end, {})
