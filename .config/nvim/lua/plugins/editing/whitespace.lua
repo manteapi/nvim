42cacfcaecf611c0eac26d8ed49b5bffbcf5976a
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2026-01-09 09:13:11 +0100

    fix(better-whitespace): do not trim xml files

--- .config/nvim/lua/plugins/editing/whitespace.lua
+++ .config/nvim/lua/plugins/editing/whitespace.lua
@@ -1,4 +1,11 @@
-vim.g.strip_whitelines_at_eof=1
+vim.g.strip_whitelines_at_eof = 1
+vim.g.strip_only_modified_lines = 1
 vim.g.strip_whitespace_on_save = 1
-vim.g.better_whitespace_enabled=0
-vim.g.strip_whitespace_confirm=0
+vim.g.better_whitespace_enabled = 0
+vim.g.strip_whitespace_confirm = 0
+
+-- I tried to use better_whitespace_filetypes_blacklist but did not manage to make it work
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "xml" },
+    command = "let b:strip_whitespace_on_save = 0"
+})
