75cbf24980b48244941b7585372b24fef310dcb7
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2026-09-10 14:41:47 +0200

    fix(colors): neogit uses tokyonight highlights

--- .config/nvim/lua/plugins/themes/tokyonight.lua
+++ .config/nvim/lua/plugins/themes/tokyonight.lua
@@ -4,6 +4,31 @@ tokyonight.setup({
     style = "night",
     transparent = true,
     dim_inactive = true,
+    on_highlights = function(hl, c)
+        hl.NeogitDiffAdd = {
+            fg = c.git_add,
+        }
+
+        hl.NeogitDiffDelete = {
+            fg = c.git_delete,
+        }
+
+        hl.NeogitDiffChange = {
+            fg = c.git_change,
+        }
+
+        hl.NeogitDiffAddHighlight = {
+            bg = c.git_add,
+        }
+
+        hl.NeogitDiffDeleteHighlight = {
+            bg = c.git_delete,
+        }
+
+        hl.NeogitDiffChangeHighlight = {
+            bg = c.git_change,
+        }
+    end,
 })
 
 vim.cmd("colorscheme tokyonight")
